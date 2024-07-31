7b6f69aae651750477939be3c7f90a74d91b518c
Clester31 <christianlester114@gmail.com>
2024-07-31 19:06:53 -0400

    Design tweaks/added responsive design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import './App.css'
 import gh_logo from './assets/github-mark-white.png';
 import li_logo from './assets/li_logo.png';
@@ -16,6 +16,7 @@ import js_logo from './assets/tool_images/js.png';
 import python_logo from './assets/tool_images/python.png';
 import react_logo from './assets/tool_images/react.png';
 import tailwind_logo from './assets/tool_images/tailwind.png';
+import mongo_logo from './assets/tool_images/mongo.png';
 
 import album_site from './assets/project_images/album-site.png'
 import crate_site from './assets/project_images/crate-site.png'
@@ -28,6 +29,7 @@ import ProjectDisplay from './components/ProjectDisplay';
 
 import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
+import { Link, Element, animateScroll as scroll } from 'react-scroll';
 import 'react-pdf/dist/Page/AnnotationLayer.css';
 import 'react-pdf/dist/Page/TextLayer.css';
 
@@ -41,15 +43,25 @@ export default function App() {
     <div className="flex flex-col min-h-screen overflow-x-hidden">
       <Navbar />
       <div className="flex-grow">
-        <Header />
-        <AboutMe />
-        <Tools />
-        <Projects />
-        <Resume />
+        <Element name="home">
+          <Header />
+        </Element>
+        <Element name="about">
+          <AboutMe />
+        </Element>
+        <Element name="tools">
+          <Tools />
+        </Element>
+        <Element name="projects">
+          <Projects />
+        </Element>
+        <Element name="resume">
+          <Resume />
+        </Element>
       </div>
       <Footer />
     </div>
-  )
+  );
 }
 
 function Navbar() {
@@ -57,14 +69,12 @@ function Navbar() {
 
   useEffect(() => {
     const updateBgOnScroll = () => {
-      if (window.scrollY > 0) {
-        setScrolled(true);
-      } else {
-        setScrolled(false);
-      }
-    }
+      setScrolled(window.scrollY > 0);
+    };
     window.addEventListener('scroll', updateBgOnScroll);
-  }, [])
+
+    return () => window.removeEventListener('scroll', updateBgOnScroll);
+  }, []);
 
   return (
     <div className={`navbar ${scrolled ? 'scrolled' : ''} hidden text-xl flex-row justify-between p-2 items-center fixed w-screen sm:flex`}>
@@ -72,25 +82,43 @@ function Navbar() {
         <h1 className='text-2xl'>CL.</h1>
       </div>
       <div className='flex flex-row'>
-        <ul className='flex flex-row p-2 ml-10'>
-          <li>Home</li>
-          <li>About</li>
-          <li>Projects</li>
-          <li>Resume</li>
+        <ul className='flex flex-row p-2 lg:ml-10'>
+          <li className='w-16 lg:text-lg text-sm lg:w-24 flex justify-center mx-6'>
+            <Link to="home" smooth={true} duration={500} offset={-100}>Home</Link>
+          </li>
+          <li className='w-16 lg:text-lg text-sm lg:w-24 flex justify-center mx-6'>
+            <Link to="about" smooth={true} duration={500} offset={-100}>About</Link>
+          </li>
+          <li className='w-16 lg:text-lg text-sm lg:w-24 flex justify-center mx-6'>
+            <Link to="projects" smooth={true} duration={500} offset={-90}>Projects</Link>
+          </li>
+          <li className='w-16 lg:text-lg text-sm lg:w-24 flex justify-center mx-6'>
+            <Link to="resume" smooth={true} duration={500} offset={-100}>Resume</Link>
+          </li>
         </ul>
       </div>
-      <div className='mx-2'>
-        <button className='aero w-16 mx-2 hover:bg-amber-500'><a href='https://github.com/Clester31' target='_blank'><img src={gh_logo} alt="" /></a></button>
-        <button className='aero w-16 mx-2 hover:bg-amber-500'><a href='https://www.linkedin.com/in/christian-lester-b71412239/' target='_blank'><img src={li_logo} alt="" /></a></button>
+      <div>
+        <ul className='flex flex-row'>
+          <li className='text-sm lg:w-16 flex justify-center mx-6 lg:flex hidden'>
+            <a href='https://github.com/Clester31' target='_blank' rel='noopener noreferrer'>
+              <img className='w-8' src={gh_logo} alt="GitHub" />
+            </a>
+          </li>
+          <li className='text-sm lg:w-16 flex justify-center mx-6 lg:flex hidden'>
+            <a href='https://www.linkedin.com/in/christian-lester-b71412239/' target='_blank' rel='noopener noreferrer'>
+              <img className='w-8' src={li_logo} alt="LinkedIn" />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
-  )
+  );
 }
 
 function Header() {
   return (
-    <div className='flex justify-center my-32 items-center'>
-      <div className='about-me flex flex-col mx-10 text-4xl leading-10 p-4 rounded-xl'>
+    <div className='flex justify-center sm:mt-32 mt-16 mb-16 items-center lg:flex-row flex-col'>
+      <div className='about-me flex flex-col mx-10 text-2xl sm:text-4xl leading-10 p-4 rounded-xl lg:mb-0 mb-10'>
         <h1>Hi there!</h1>
         <h1>My name is <span className='name-text font-bold'>Christian Lester</span></h1>
       </div>
@@ -105,12 +133,12 @@ function AboutMe() {
   const [imgGallery, setImgGallery] = useState([img_01, img_02]);
 
   return (
-    <div className='about-me flex flex-col items-center w-1/2 items-center justify-center m-auto rounded-xl p-2'>
+    <div className='about-me flex flex-col items-center w-3/5 items-center justify-center m-auto rounded-xl p-2 text-center'>
       <h1 className='text-3xl my-6'>About Me</h1>
-      <div className='flex flex-row justify-around'>
-        <div className='leading-8 p-2 text-lg'>
+      <div className='flex flex-row justify-around mb-4'>
+        <div className='leading-8 p-2 text-md'>
           <h1>I am a currently a student at the University of Pittsburgh pursuing a degree in Computer Science.</h1>
-          <h1>I have over six years of programming experience with lagnauges such as Java, C, C++, Python, and JavaScript.</h1>
+          <h1>I have over six years of programming experience with Java, C, C++, Python, and JavaScript, etc.</h1>
           <h1>Currently, I am aspiring to become a front-end developer with a primary focus on React.</h1>
           <h1>Outside of programming, I also enjoy music production as well. <a href='https://open.spotify.com/artist/6i8lLCMSQ1pBgIt2B5XH3e?si=WyII-thIS5qOMkIHPjuCJw' target='_blank' className='text-white hover:text-black'>Check it out here!</a></h1>
         </div>
@@ -120,15 +148,17 @@ function AboutMe() {
 }
 
 function Tools() {
-  const [langIcons, setLangIcons] = useState([c_logo, cpp_logo, java_logo, python_logo, html_logo, css_logo, js_logo, react_logo, tailwind_logo]);
+  const [langIcons, setLangIcons] = useState([c_logo, cpp_logo, java_logo, python_logo, html_logo, css_logo, js_logo, react_logo, tailwind_logo, mongo_logo]);
 
   return (
-    <div className='about-me flex flex-col items-center w-1/2 items-center justify-center m-auto rounded-xl p-2 mt-10'>
+    <div className='about-me flex flex-col items-center w-3/5 justify-center m-auto rounded-xl p-2 mt-10'>
       <h1 className='text-3xl my-6'>Languages/Tools</h1>
-      <div>
-        {langIcons.map((item, idx) => {
-          return <button key={idx} className='aero tool-icon'><img src={item} alt="" /></button>
-        })}
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-5 gap-4 flex mb-6'>
+        {langIcons.map((item, idx) => (
+          <button key={idx} className='aero tool-icon p-2'>
+            <img src={item} alt="" className='w-12' />
+          </button>
+        ))}
       </div>
     </div>
   )
@@ -136,9 +166,9 @@ function Tools() {
 
 function Projects() {
   return (
-    <div className='about-me flex flex-col items-center w-1/2 items-center justify-center m-auto rounded-xl p-2 mt-10'>
+    <div className='about-me flex flex-col items-center w-3/5 items-center justify-center m-auto rounded-xl p-2 mt-10'>
       <h1 className='text-3xl my-6'>Projects</h1>
-      <div className='flex flex-row'>
+      <div className='flex flex-col lg:flex-row '>
         <ProjectDisplay image={album_site} description={'A site where users can rate, review, and maintain a collection of albums.'} link={'https://github.com/Clester31/album-review-site'} />
         <ProjectDisplay image={crate_site} description={'A loot box opening simulator based off Team Fortress 2.'} link={'https://github.com/Clester31/album-review-site'} />
         <ProjectDisplay image={color_site} description={'A simple color gradient generator that creates a seamless transition.'} link={'https://github.com/Clester31/Color-Gradient-Generator'} />
@@ -149,10 +179,10 @@ function Projects() {
 
 function Resume() {
   return (
-    <div className='about-me flex flex-col items-center w-1/2 items-center justify-center m-auto rounded-xl p-2 mt-10'>
+    <div className='about-me flex flex-col items-center w-3/5 items-center justify-center m-auto rounded-xl p-2 mt-10'>
       <h1 className='text-3xl my-6'>Resume</h1>
       <button className='aero mb-6 hover:bg-amber-500'><a href={resume} download={'Christian Lester Resume'}>Download PDF</a></button>
-      <div className='shadow-xl'>
+      <div className='shadow-xl lg:flex hidden'>
         <Document file={resume}>
           <Page pageNumber={1} renderMode='canvas' />
         </Document>
--- src/components/ProjectDisplay.jsx
+++ src/components/ProjectDisplay.jsx
@@ -2,9 +2,9 @@ import '../App.css'
 
 export default function ProjectDisplay({ image, description, link }) {
     return (
-        <div className='project text-center m-auto border-2 rounded-xl p-3 w-1/3 mb-5 h-96 mx-2'>
-            <img src={image} alt="" className='w-64 h-50 border-2 rounded-xl flex m-auto' />
-            <p className='w-1/2 flex m-auto pt-2'>{description}</p>
+        <div className='project text-center m-auto border-2 rounded-xl p-3 lg:w-1/3 mb-5 h-96 mx-2'>
+            <img src={image} alt="" className='w-64 h-64border-2 rounded-xl flex m-auto h-1/2' />
+            <p className='flex m-auto mt-2 h-1/4'>{description}</p>
             <button className='aero hover:bg-amber-500 my-2'><a href={link} target='_blank'>See Project</a></button>
         </div>
     )
